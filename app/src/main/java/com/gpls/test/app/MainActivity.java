97034cb889c9b42752d4d720c0dce39690856008
realitygaps <github@gapsinreality.com>
2014-03-08 22:10:46 +0100

    Initial commit

---
+++ app/src/main/java/com/gpls/test/app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.gpls.test.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("http://m.9292.nl");
+    }
+
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
+    }
+
+}
